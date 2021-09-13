16be5ebb8a712287fb88cb6deb558819e40dab7a
Amir Yavand <a.yavand@persiangig.com>
2021-09-13 07:20:40 +0430

    initial configurations & cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/normalize.css';
+import '../styles/globals.css';
+
+import { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
